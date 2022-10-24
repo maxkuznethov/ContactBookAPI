56dfe8da8147c2871603ef999b97c9bc17f4be51
Maxim Kuznethov <max.kuznethov03@gmail.com>
2022-10-24 23:52:48 +0300

    fix blank string check

--- src/main/java/ru/mirea/contact_book/services/ContactService.java
+++ src/main/java/ru/mirea/contact_book/services/ContactService.java
@@ -1,14 +1,12 @@
 package ru.mirea.contact_book.services;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import ru.mirea.contact_book.entities.Contact;
 import ru.mirea.contact_book.exceptions.MyEntityNotFoundException;
 import ru.mirea.contact_book.repositories.ContactRepository;
 
-import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @Service
@@ -29,12 +27,11 @@ public class ContactService {
     }
 
     public Contact updateContact(Long id, Contact contact) {
-        Contact contactToUpdate = contactRepository.findById(id)
-                .orElseThrow(() -> new MyEntityNotFoundException(id));
-        if (contact.getName()!=null && !contact.getName().isEmpty()) {
+        Contact contactToUpdate = contactRepository.findById(id).orElseThrow(() -> new MyEntityNotFoundException(id));
+        if (StringUtils.isNotBlank(contact.getName())) {
             contactToUpdate.setName(contact.getName());
         }
-        if (contact.getPhone()!=null && !contact.getPhone().isEmpty()) {
+        if (StringUtils.isNotBlank(contact.getPhone())) {
             contactToUpdate.setPhone(contact.getPhone());
         }
         return contactRepository.save(contactToUpdate);
